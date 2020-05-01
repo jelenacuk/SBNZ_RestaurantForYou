c539133eecf7e4ba929265c2defd4ec27466edf4
jelenacuk <jelena.cuk.1997@gmail.com>
2020-05-01 16:53:09 +0200

    initial commit

---
+++ RestaurantForYou/src/main/java/com/sbnz/RestaurantForYou/RestaurantForYouApplication.java
@@ -0,0 +1,13 @@
+package com.sbnz.RestaurantForYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantForYouApplication.class, args);
+	}
+
+}
---
+++ RestaurantForYou/src/test/java/com/sbnz/RestaurantForYou/RestaurantForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbnz.RestaurantForYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantForYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
