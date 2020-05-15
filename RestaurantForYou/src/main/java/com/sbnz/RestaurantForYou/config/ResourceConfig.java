900e20f35a7245dd5b5bc4d2d3f74d123ce57e65
jelenacuk <jelena.cuk.1997@gmail.com>
2020-05-16 01:13:02 +0200

    Drools configuration

--- RestaurantForYou/src/main/java/com/sbnz/RestaurantForYou/config/ResourceConfig.java
+++ RestaurantForYou/src/main/java/com/sbnz/RestaurantForYou/config/ResourceConfig.java
@@ -1,4 +1,4 @@
-package com.sbnz.RestaurantForYou.security;
+package com.sbnz.RestaurantForYou.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
